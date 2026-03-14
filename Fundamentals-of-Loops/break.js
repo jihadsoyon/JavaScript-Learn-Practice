c7dbfbcb11eb11957d9dd5cb423689c93df92ea8
jihadsoyon <jihadsoyon@gmail.com>
2026-03-14 21:17:41 +0600

    practice break & continue technique also explore do while technique

---
+++ Fundamentals-of-Loops/break.js
@@ -0,0 +1,23 @@
+
+//for system
+for (let i = 0; i < 150; i++){
+  console.log(i);
+  if(i === 5) {
+    break;
+  }
+}
+
+console.log('life after breakup')
+
+// while system
+let n= 54;
+while(n > 25){
+  console.log(n);
+  if(n < 50){
+    break;
+  }
+  n--;
+}
+
+
+
