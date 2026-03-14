fb9f2b556dae2d06344c4e8815a0bcc688c25fa4
jihadsoyon <jihadsoyon@gmail.com>
2026-03-14 20:57:21 +0600

    learnd & practice for of loop

---
+++ Fundamentals-of-Loops/for-of.js
@@ -0,0 +1,22 @@
+
+//FOR LOOP
+// while.js
+
+
+const numbers = [1, 98, 45, 63, 2, 72];
+
+console.log(numbers);
+for(const num of numbers){
+  console.log(num)
+}
+
+const fruits = ['orange', 'apple', 'jackfruit', 'watermelon']
+
+for(const fruit of fruits){
+  console.log(fruit);
+  console.log('I want to eat')
+}
+
+
+
+
