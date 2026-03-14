c6399c2e8b8344879503819e41df26efa905be74
jihadsoyon <jihadsoyon@gmail.com>
2026-03-14 21:06:50 +0600

    practice odd, even number using loop & modulas

---
+++ Fundamentals-of-Loops/odd-even.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i < 10; i++){
+//   console.log(i);
+// }
+
+// // even
+
+// for (let i = 2; i <= 20; i=i+2){
+//   console.log(i);
+// }
+
+// //odd
+// for (let i =1; i < 20; i=i+2){
+//   console.log(i);
+// }
+
+// // practice
+
+// for (var i=0; i<5; i++){
+//   console.log('Hello');
+// }
+
+// for(let i =0; i < 10; i++){
+//   console.log(i)
+// }
+
+for(let i = 2; i <= 20; i+=2){
+  console.log(i)
+}
+
+for(let i =1; i < 20; i+=2){
+  console.log(i)
+}
+
+for(let i = 0; i < 5; i++){
+  console.log('Hello');
+}
