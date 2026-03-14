8dcced530c8cc7773ced8f623043bec9d289e43f
jihadsoyon <jihadsoyon@gmail.com>
2026-03-14 21:11:36 +0600

    learn decremental technique & incremental technique using for & while loop

---
+++ Fundamentals-of-Loops/decremental.js
@@ -0,0 +1,20 @@
+//incremental for loop
+// for( let i = 0; i <= 10; i++){
+//   console.log(i);
+// }
+
+
+
+//decremental for looop
+//for loop
+for (let i = 10; i >= 0; i--){
+  console.log(i);
+}
+
+
+//Using while loop
+let n = 10;
+while(n >= 0){
+  console.log(n);
+  n--;  // n= n-1
+}
---
+++ Fundamentals-of-Loops/for-sum.js
@@ -0,0 +1,17 @@
+// let sum = 0;
+// for(let i = 11; i <= 20; i++){
+//   sum = sum + i;
+//   console.log(i);
+// }
+
+// console.log('sum of numbers:', sum);
+
+
+let sum =0;
+for(let i=11; i <= 20; i++){
+console.log(i);
+sum = sum + i;
+
+}
+
+console.log('sum of i', sum);
