747ad02fff489b048b3ec03a6385f8f6711e590e
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:38:20 +0600

    implement JS string methods and basic conditions

---
+++ JS-Strings-Objects/case.js
@@ -0,0 +1,27 @@
+const school = 'RAJ UK Uttara Model School';
+console.log(school)
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if( subject.toLowerCase() === book.toLowerCase()){
+  console.log('I am reading book aibar porikkhay fatai felmu');
+}
+else{
+  console.log('hudai hudai pristha ultay');
+}
+
+const drink = ' water ';
+const liquid = '   water  '
+if(drink.trim() === liquid.trim()){
+  console.log('panir opor nam life');
+}
+else{
+  console.log('somudre pani ase khayte pari na');
+}
+
+
