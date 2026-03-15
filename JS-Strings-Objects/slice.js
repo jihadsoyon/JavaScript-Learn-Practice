ee778e426fb59709b7c3d5fd10a57350e78264b0
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:40:58 +0600

    practice string slicing, splitting, and joining in JavaScript

---
+++ JS-Strings-Objects/slice.js
@@ -0,0 +1,54 @@
+const address = 'andorkilla';
+const part = address.slice(2, 5);
+console.log(part); 
+
+const lili = 'Coxsbazar';
+const pat = lili.slice(0, 4);
+console.log(pat);
+
+
+const sentence = 'I am a good and hardworking person.';
+// console.log(sentence.split());
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+const friendsStr = 'Rahim, kahim, dahim, lahim, fahim, sahim';
+// console.log(friendsStr);
+const friends = friendsStr.split(',');
+// console.log(friends)
+
+
+
+const realfrined = [ 'Rahim', ' kahim', ' dahim', ' lahim', ' fahim', ' sahim' ];
+// console.log(realfrined);
+console.log(realfrined.join(''));
+// console.log(realfrined.join('|')) ;
+// console.log(realfrined.join('-'));
+
+
+
+
+
+const namee = 'Bangladesh';
+const sliting = namee.slice(0,4);
+console.log(sliting);
+
+
+
+const bakko = 'I wil try and try try untill i dead';
+console.log(bakko.split(' '))
+console.log(bakko.split())
+
+const folMul = ['am', 'jam', 'kathal', 'lichu', 'peyara'];
+console.log(folMul) 
+console.log(folMul.join(" "))
+console.log(folMul.join('|'))
+
+
+
+
+
+
+
+
+
