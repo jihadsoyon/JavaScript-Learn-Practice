d55099318f136e72e896aa679513c41d427b83c6
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:57:23 +0600

    implement Object.keys to enumerate properties of key objects

---
+++ JS-Strings-Objects/keys.js
@@ -0,0 +1,54 @@
+const computer = {
+  brand: 'lenovo',
+  price: 35000,
+  processor: 'intel',
+  hdd: '512gb',
+  monitor: 'hp'
+}
+
+const keys = Object.keys(computer);
+console.log(keys)
+
+
+const programming ={
+  brand: 'code',
+  fast: 'HTML',
+  last: 'CSS',
+  coder: 333333,
+  hevi: 'Javascript'
+}
+
+const key = Object.keys(programming);
+console.log(key);
+
+
+const skills = {
+  markup: 'HTML',
+  style: 'CSS',
+  programming: 'JS', 
+  library: 'React'
+}
+const skill = Object.keys(skills);
+console.log(skill);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
