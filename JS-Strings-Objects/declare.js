d56dae4f260079be002d9df8bcd5fb3b29103f43
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 22:02:35 +0600

    Add examples of object creation: object literal, constructor, and Object.create

---
+++ JS-Strings-Objects/declare.js
@@ -0,0 +1,13 @@
+const pen = {brand: 'econo', price: 10, color: 'black'}
+console.log(pen)
+
+const pencil = new Object();
+pencil.brand = "Matador";
+pencil.price = 5;
+console.log(pencil);
+
+
+
+const rubber = Object.create({})
+console.log(rubber)
+
