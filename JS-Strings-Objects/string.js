7f53e73860d7c0e893c438885979fc64b7f2680f
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:35:06 +0600

    Explored string immutability: accessing characters and attempted modification

---
+++ JS-Strings-Objects/string.js
@@ -0,0 +1,37 @@
+
+
+const country = 'bangladesh';
+const division = "Newkhale";
+const district = `B-baria`;
+const thana = new String(`Demra`);
+// console.log(country);
+
+//array is mutable ---> you can change the array elements
+const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1]= 11;
+// console.log(numbers);
+
+
+//string is immutable ----> you can't change the string elements
+const capital = 'Dhaka';
+console.log(capital.length);
+console.log(capital[3]);
+console.log(capital);
+capital[0]= 'F';
+console.log(capital);
+
+
+
+
+
+
+
+
+
+
+
+
+
