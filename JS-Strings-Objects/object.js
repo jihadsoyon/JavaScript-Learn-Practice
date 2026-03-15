2093c4a5e6b9164faba0c9803a821435859bbe51
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:51:05 +0600

    practice: define primitive and non-primitive data types with objects and arrays

---
+++ JS-Strings-Objects/object.js
@@ -0,0 +1,40 @@
+
+//primitive
+//basic, primary datatype
+
+const age = 21;
+const school = 'light house';
+const isPassed = true;
+//let isDeveloper;
+
+const subjects = ['Bangla', 'English', 'Physics', 'Math']
+const bottle_wrong = ['white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+console.log(bottleDrinks);
+
+//object
+//non-primitive--------------->
+
+
+const bottle  = {
+  brand: 'apple',
+  price: 45,
+  color: 'white',
+  isClean: false
+}
+console.log(bottle)
+
+const subject = {
+  name: 'biology',
+  teacher: 'rasheda mam',
+  examDate: '30 february',
+  chapters: ['first', 'second', 'third'],
+  exams: {
+    name: 'Final Exam',
+    marks: 100
+  }
+}
+console.log(subject)
