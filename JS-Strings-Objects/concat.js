0211b1e9266cfbfca8a7488c1f8a43576ec4d93c
jihadsoyon <jihadsoyon@gmail.com>
2026-03-15 21:46:18 +0600

    Demonstrate string concatenation methods and character presence checks

---
+++ JS-Strings-Objects/concat.js
@@ -0,0 +1,36 @@
+const first = 'Abid';
+const last = 'Nabid';
+
+// const fullname = first + last;
+
+const fullname = first + ' ' + last;
+// console.log(fullname);
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
+
+//includes
+
+console.log(last.includes('d'))
+
+
+
+
+
+
+const fast = 'Jihad';
+const lastt = 'Soyon';
+
+const puraname = fast.concat('       ').concat(lastt);
+console.log(puraname)
+console.log(lastt.includes('o'))
+
+
+
+const pothom = 'Amit';
+const dityo = 'Ahmed';
+const tityo = 'Munir';
+const puroName = pothom.concat(' ').concat(dityo).concat(' ').concat(tityo);
+console.log(puroName);
+
+
+
