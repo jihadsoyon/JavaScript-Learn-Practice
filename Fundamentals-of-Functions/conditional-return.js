39bed05601fc5b43080f9f20d1a49446da2ea912
jihadsoyon <jihadsoyon@gmail.com>
2026-03-19 21:51:38 +0600

    add JavaScript practice functions for odd/even checks and conditional logic

---
+++ Fundamentals-of-Functions/conditional-return.js
@@ -0,0 +1,41 @@
+function isOdd(number){
+  if(number % 2 === 1){
+    return true;
+  }
+  return false;
+}
+console.log(isOdd(10));
+console.log(isOdd(7));
+
+//prctice 01
+function hisLo (number){
+  if(number % 2 === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log(hisLo(9));
+console.log(hisLo(20));
+
+//prctice 02
+function tyrNre (number){
+  if (number -2 === 6){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log(tyrNre(8));
+console.log(tyrNre(10));
+
+
+
+
+
+
+
+
+
