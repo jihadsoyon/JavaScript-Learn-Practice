43a93abfe6b3e8127dec56c3d66b73c59e5f6765
jihadsoyon <jihadsoyon@gmail.com>
2026-03-19 21:43:31 +0600

    practice parameter in function

---
+++ Fundamentals-of-Functions/parameter.js
@@ -0,0 +1,95 @@
+function vaatKhao(){
+  console.log('hand wash kore asho');
+  console.log('boso');
+  console.log('plate a khabar dhabar nao');
+  console.log('gopat gopat kore khao');
+}
+
+function kolaKhaw(){
+  console.log('khau');
+  console.log('khau');
+  console.log('khau');
+  console.log('khau');
+}
+// kolaKhaw();
+
+// vaatKhao();
+
+
+//Borgo
+ function square(number){
+  console.log('value of the number parameter', number);
+  const borgo = number * number ;
+  console.log('square of the given number is: ', borgo);
+ }
+ square(5);
+//  square (4);
+//  square (8);
+//  square (68);
+
+
+//Sum
+function add(num1, num2){
+  const sum = num1 + num2;
+  // console.log(sum);
+}
+
+// add(6, 67);
+// add(890, 110);
+
+
+//Sum
+function addAll(a, b, c, d, e){
+  const total = a + b+ c + d + e;
+  console.log(a, b, c, d, e);
+  console.log(total);
+}
+ addAll(4, 3, 2, 1, 1000)
+
+//minus
+function newAdd(notun1, notun2){
+  const mot = notun1-notun2;
+  console.log(mot);
+}
+newAdd(500, 10);
+
+
+//practice 01
+
+function jogajog(jog1, jog2, jog3, jog4){
+  const shobmilaye = jog1 + jog2 + jog3 + jog4 ;
+  console.log(shobmilaye);
+}
+
+jogajog(15, 15, 15, 15) ;
+
+
+
+
+
+function jaAse(a, b, c, d, e){
+  const shuru = a+b+c+d+e ;
+  // console.log(a, b, c, d, e);
+  // console.log(shuru);
+}
+jaAse(2,2,2,2,4);
+
+
+
+function biyogKor(a, b, c, d){
+  const biyog = a-b-c-d ;
+
+  // console.log('biyogfol', biyog);
+}
+
+biyogKor(2, 2, 2, 2);
+
+
+function vagKor(a, b){
+  const vag = a / b ;
+  console.log('vag er mal', a,b);
+  console.log('vagfol', vag) ;
+}
+
+vagKor(200, 5);
+
