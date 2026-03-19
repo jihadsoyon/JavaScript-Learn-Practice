e18fe62f300dae1d2477c3271e60583178d1d88a
jihadsoyon <jihadsoyon@gmail.com>
2026-03-19 21:46:41 +0600

    practice JavaScript functions using return values

---
+++ Fundamentals-of-Functions/return.js
@@ -0,0 +1,52 @@
+function tenTimes(number){
+  const result = number * 10;
+  return result;
+}
+
+function cuthalf(number){
+  const half =  number / 2;
+  return half;
+}
+
+function bisMillah(number){
+  const utor = number + 70;
+  return utor;
+}
+
+function allhuAkbar(number){
+  const niyom = number * 100;
+  return niyom;
+}
+
+const output = tenTimes(5);
+console.log('output', output)
+
+const bigNumber = cuthalf(99);
+console.log('bignumber', bigNumber)
+
+const notunNumber = bisMillah(80);
+console.log('notunNumber', notunNumber)
+
+const notunNiyom = allhuAkbar(33);
+console.log('notunNiyom', notunNiyom)
+
+
+
+
+//practice 01
+
+function aulaJhaula(number){
+  const eije = number - 40 ;
+  return eije;
+}
+const termi = aulaJhaula(100);
+console.log('outPut', termi);
+
+//practice 02
+function hoilotaile(number){
+  const eilo = number / 2 ;
+  return eilo;
+}
+
+const nara = hoilotaile(200);
+console.log('narachara', nara)
