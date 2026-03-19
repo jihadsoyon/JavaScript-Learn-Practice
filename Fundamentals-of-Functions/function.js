77a59ee14702738c814620d6daf30a4e7fe4e223
jihadsoyon <jihadsoyon@gmail.com>
2026-03-19 21:42:06 +0600

    learn & practice basic function

---
+++ Fundamentals-of-Functions/function.js
@@ -0,0 +1,46 @@
+
+//01
+function fannoffKor(){
+  console.log('bosa theke uth')
+  console.log('switch er one jah')
+  console.log('fann de')
+}
+
+fannoffKor();  //call the function;
+
+
+
+//02
+function brushTooth(){
+console.log('pick up the brush');
+console.log('add paste')
+console.log('2 min gosho gosho and fena felo')
+
+}
+
+brushTooth();
+
+//03
+function brusthTeeth(){
+  console.log('pick up the brush')
+  console.log('add paste')
+  console.log('2 min ghosho and ghoshso and fena tolo')
+}
+
+// call the function
+
+brusthTeeth();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
