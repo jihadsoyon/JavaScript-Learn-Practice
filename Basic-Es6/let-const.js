5a184299f7c28dd8b2318fb244ba6d1336057076
jihadsoyon <jihadsoyon@gmail.com>
2026-04-02 13:05:00 +0600

    demonstrate differences between var, let, and const with examples

---
+++ Basic-Es6/let-const.js
@@ -0,0 +1,28 @@
+const name = 'Bangladesh';
+// name = 'Singapore';   // const diye declare korle 
+console.log(name)
+
+let price = 120;
+price = 150;
+console.log(price);
+
+
+//Interview Question
+// Var, let, const er difference ki
+
+
+var a = 10;
+var a = 20; // এটা re-declare
+
+let b = 10;
+b = 20; // এটা re-assign
+
+
+
+// ⚡ var, let, const এ কী হয়?
+// Keyword	Re-declare	Re-assign
+// var	    ✅ Yes	     ✅ Yes
+// let	    ❌ No	     ✅ Yes
+// const    ❌ N        ❌ No
+
+
