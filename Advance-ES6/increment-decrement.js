347a3b7587f7ef06697149addfd1aefbaceec8e0
jihadsoyon <jihadsoyon@gmail.com>
2026-04-06 15:58:16 +0600

    Practice increment and decrement operators (pre vs post)

---
+++ Advance-ES6/increment-decrement.js
@@ -0,0 +1,36 @@
+
+//increment 
+let a = 5;
+a++;
+// console.log(a++);
+console.log(++a);
+console.log(a);
+
+//decrement
+
+let b = 10;
+b--;
+console.log(--b);
+console.log(b--);
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
