d0fcb1366e27a1e166b00f35a4c30cf7d612a9ea
jihadsoyon <jihadsoyon@gmail.com>
2026-07-10 11:41:53 +0600

    add cashout functionality & declare some common function for whole project

---
+++ Payoo-Project/script/machine.js
@@ -0,0 +1,9 @@
+//machine id --> input value
+
+function getvalueFromInput(id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(id, value);
+    return value;
+}
+
