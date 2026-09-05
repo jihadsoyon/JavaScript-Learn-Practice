ff53eae5f25098f64c62915277bf703d74ccd051
jihadsoyon <jihadsoyon@gmail.com>
2026-09-05 18:20:20 +0600

    practice: solve reduce challenges for even sum, salary and cart total

--- Advance-ES6/reduce.js
+++ Advance-ES6/reduce.js
@@ -1,10 +1,10 @@
-const numbers = [1, 2, 3, 4, 5];
+const numberss = [1, 2, 3, 4, 5];
 let sum = 0;
-for(const num of numbers){
+for(const num of numberss){
 sum = sum + num;
 }
 
-const total = numbers.reduce((acc, num) => acc + num, 0)
+const total = numberss.reduce((acc, num) => acc + num, 0)
 console.log(sum);
 console.log(total)
 
@@ -13,23 +13,62 @@ console.log(total)
 
 
 
+const numbers = [10, 15, 20, 25, 30, 35];
+
+const result = numbers.reduce((acc, num) => {
+    if (num % 2 === 0) {
+        return acc + num;
+    }
+
+    return acc;
+}, 0);
+
+console.log(result);
+
+
+
+
+const employees = [
+    { name: "Jihad", salary: 50000 },
+    { name: "Rahim", salary: 40000 },
+    { name: "Karim", salary: 60000 },
+    { name: "Hasan", salary: 30000 }
+];
+
+
+
+const totalSalary = employees.reduce((acc, employee) => {
+    return acc + employee.salary;
+}, 0);
+
+console.log(totalSalary);
 
 
 
 
 
+const cart = [
+    {
+        name: "Laptop",
+        price: 80000,
+        quantity: 1
+    },
+    {
+        name: "Mouse",
+        price: 1500,
+        quantity: 2
+    },
+    {
+        name: "Keyboard",
+        price: 3000,
+        quantity: 1
+    }
+];
 
+const totall = cart.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+}, 0);
 
-
-
-
-
-
-
-
-
-
-
-
+console.log(totall);
 
 
